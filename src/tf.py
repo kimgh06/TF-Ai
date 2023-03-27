f23dec96c3bbbcb6fd2d49be43ac4e48a8e34496
kkh061101 <kkh061101@naver.com>
2023-03-27 23:38:22 +0900

    모델 구조 함수 제작

--- src/tf.py
+++ src/tf.py
@@ -1,32 +1,38 @@
 # 파일로부터 데이터 읽어오기
 import pandas as pd
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/lemonade.csv'
-레모네이드 = pd.read_csv(파일경로)
+import tensorflow as tf
+model = 0
+레모네이드 = pd.read_csv(
+    'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/lemonade.csv')
 
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv'
-보스턴 = pd.read_csv(파일경로)
+보스턴 = pd.read_csv(
+    'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv')
 
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/iris.csv'
-아이리스 = pd.read_csv(파일경로)
+아이리스 = pd.read_csv(
+    'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/iris.csv')
 
-###########################
-# 데이터의 모양확인
-print(레모네이드.shape)
-print(보스턴.shape)
-print(아이리스.shape)
+print(레모네이드.shape, 레모네이드.columns)
+print(보스턴.shape, 보스턴.columns)
+print(아이리스.shape, 아이리스.columns)
 
-###########################
-# 데이터 칼럼이름 확인
-print(레모네이드.columns)
-print(보스턴.columns)
-print(아이리스.columns)
-
-
-###########################
-# 독립변수와 종속변수 분리
 독립 = 레모네이드[['온도']]
 종속 = 레모네이드[['판매량']]
 print(독립.shape, 종속.shape)
+
+
+def model_structure(x, y):
+    X = tf.keras.layers.Input(shape=[x])
+    Y = tf.keras.layers.Dense(y)(X)
+    global model
+    model = tf.keras.models.Model(X, Y)
+    model.compile(loss="mse")
+    model.fit(독립, 종속, epochs=10000, verbose=1)
+    model.fit(독립, 종속, epochs=10)
+
+
+model_structure(1, 1)
+print(model.predict(독립))
+print(model.predict([[15]]))
 
 독립 = 보스턴[['crim', 'zn', 'indus', 'chas', 'nox',
           'rm', 'age', 'dis', 'rad', 'tax',
@@ -34,13 +40,16 @@ print(독립.shape, 종속.shape)
 종속 = 보스턴[['medv']]
 print(독립.shape, 종속.shape)
 
+model_structure(13, 1)
+print(model.predict(독립[5:10]))
+print(종속[5:10])
+print(model.get_weights())
+
 독립 = 아이리스[['꽃잎길이', '꽃잎폭', '꽃받침길이', '꽃받침폭']]
 종속 = 아이리스[['품종']]
 print(독립.shape, 종속.shape)
 
 
-###########################
-# 각각의 데이터 확인해보기
 print(레모네이드.head())
 print(보스턴.head())
 print(아이리스.head())
