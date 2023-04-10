07a312bf6d6d5366b0fb8d657480806b466eab3a
kkh061101 <kkh061101@naver.com>
2023-04-10 19:28:18 +0900

    Update img.py

--- imgtest/img.py
+++ imgtest/img.py
@@ -1,38 +1,53 @@
+###########################
+# 라이브러리 사용
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
-(trX, trY), (teX, teY) = tf.keras.datasets.mnist.load_data()
-print(trX.shape, trY)
-(ctrX, ctrY), (cteX, cteY) = tf.keras.datasets.cifar10.load_data()
-print(ctrX.shape, ctrY)
+import pandas as pd
 
-# print(trY[0:10])
-# plt.imshow(trX[0], cmap='gray')
-# plt.show()
-# print(trY[0])
+###########################
+# with reshape
 
-# print(ctrY[0:10])
-# plt.imshow(ctrX[0])
-# plt.show()
+# 데이터를 준비하고
+(독립, 종속), _ = tf.keras.datasets.mnist.load_data()
+독립 = 독립.reshape(60000, 784)
+종속 = pd.get_dummies(종속)
+print(독립.shape, 종속.shape)
 
-dl = np.array([1, 2, 3, 4, 5])
-print(dl.shape)
+# 모델을 만들고
+X = tf.keras.layers.Input(shape=[784])
+H = tf.keras.layers.Dense(84, activation='swish')(X)
+Y = tf.keras.layers.Dense(10, activation='softmax')(H)
+model = tf.keras.models.Model(X, Y)
+model.compile(loss='categorical_crossentropy', metrics='accuracy')
 
-dl = np.array([dl, dl, dl, dl])
-print(dl.shape)
+# 모델을 학습하고
+model.fit(독립, 종속, epochs=10)
 
-dl = np.array([dl, dl, dl, dl, dl])
-print(dl.shape)
+# 모델을 이용합니다.
+pred = model.predict(독립[0:5])
+print(pd.DataFrame(pred).round(2))
+print(종속[0:5])
 
-x1 = np.array([1, 2, 3, 4, 5])
-print(x1.shape)
-print(trY[0:5])
-print(trY[0:5].shape)
+###########################
+# with flatten
 
-x2 = np.array([[1, 2, 3, 4, 5]])
-print(x2.shape)
+# 데이터를 준비하고
+(독립, 종속), _ = tf.keras.datasets.mnist.load_data()
+# 독립 = 독립.reshape(60000, 784)
+종속 = pd.get_dummies(종속)
+print(독립.shape, 종속.shape)
 
-x3 = np.array([[1], [2], [3], [4], [5]])
-print(x3.shape)
-print(ctrY[0:5])
-print(ctrY[0:5].shape)
+# 모델을 만들고
+X = tf.keras.layers.Input(shape=[28, 28])
+H = tf.keras.layers.Flatten()(X)
+H = tf.keras.layers.Dense(84, activation='swish')(H)
+Y = tf.keras.layers.Dense(10, activation='softmax')(H)
+model = tf.keras.models.Model(X, Y)
+model.compile(loss='categorical_crossentropy', metrics='accuracy')
+
+# 모델을 학습하고
+model.fit(독립, 종속, epochs=10)
+
+# 모델을 이용합니다.
+pred = model.predict(독립[0:5])
+print(pd.DataFrame(pred).round(2))
+print(종속[0:5])
